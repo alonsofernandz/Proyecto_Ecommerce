075736e83522fa8321d625ac86c5dcf894e09cf4
Alonso <66333686+AlonsoFJ26@users.noreply.github.com>
2022-11-23 16:49:39 -0500

    Avance del Ecommerce Final

---
+++ back.js
@@ -0,0 +1,7 @@
+//Capturar nombre de usuario
+
+function nameusuario(){
+    let nom = document.getElementById("name").value;
+    let pass = document.getElementById("pasww").value;
+    alert("Bienvenido " + nom);
+}
